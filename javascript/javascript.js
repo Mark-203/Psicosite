1a7746a5aea9463167f8c3687c44beadcef69a64
Mark <57268397+Mark-203@users.noreply.github.com>
2026-05-30 20:08:44 +0200

    ciao

--- javascript/javascript.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    // Al clic sull'icona, mostra o nascondi il menu
-    $(".menu-icon-bar").click(function(e) {
-      $(".menu-categorie").toggleClass('espansione');
-      e.preventDefault();
-    });
-  });
